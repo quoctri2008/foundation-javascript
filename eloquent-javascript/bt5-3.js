108f411fa9f6b6c238b2328f3ee59fc04ada98ec
quoctri <quoctri200888@gmail.com>
2021-01-07 20:47:38 +0700

    map & filter

---
+++ eloquent-javascript/bt5-3.js
@@ -0,0 +1,9 @@
+const array = [];
+for (let i = 1; i < 21; i++) {
+    array.push(i);
+}
+const newArray = array.map(arr => arr * 7)
+console.log('Map array: ', newArray);
+
+const filterArray = newArray.filter(num => num % 2 == 0)
+console.log('Filter array: ', filterArray);
